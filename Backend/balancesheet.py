3c16b6778038daba5bfcf42eee63d10d374ef51f
Shankar <shankan@microsoft.com>
2018-05-30 00:36:25 +0530

    Initial Commit

---
+++ Backend/balancesheet.py
@@ -0,0 +1,11 @@
+class BalanceSheet:
+
+    def __init__(self):
+        self.cash_assets = 0
+        self.fixed_assets = 0
+        self.depreciation = 0
+
+
+    def calculate_net_assets(self):
+
+
---
+++ Backend/createscripts.py
@@ -0,0 +1,24 @@
+
+
+class CreateScripts:
+    """
+    This is the base class for Creating the Scripts
+    """
+
+    def __init__(self) -> None:
+        """
+        This
+
+        Args:
+            self: This is the class method name
+
+        Returns:
+
+        """
+        self.name = "Shankar"
+
+    def initiate(self):
+
+        print(self.name)
+
+
